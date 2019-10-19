0be3912d852e179e54212a7e5d1b7f4b8af92e60
Teo Morell <teomorell@hotmail.com>
2019-10-18 20:39:44 -0400

    Get sibling packages (#9)

--- test/npm/getInstalledPackages.spec.ts
+++ test/npm/getInstalledPackages.spec.ts
@@ -13,7 +13,7 @@ after(() => {
     sinon.restore();
 });
 
-test("Get packages, empty package.json", async (t) => {
+test.serial("Get packages, empty package.json", async (t) => {
     // Arguments
     sinon.stub(require("../../src/program"), "args").value({ production: undefined, development: undefined });
 
@@ -28,7 +28,7 @@ test.serial("Get all packages, full installation", async (t) => {
 
     const packages = await getInstalledPackages(path.join(__dirname, "..", "mock-packages", "installation-full"));
 
-    t.is(packages.length, 7);
+    t.is(packages.length, 11);
     t.is(packages[0].name, "prod-01");
     t.is(packages[0].version, "1.0.0");
     t.is(packages[1].name, "prod-02");
@@ -37,12 +37,20 @@ test.serial("Get all packages, full installation", async (t) => {
     t.is(packages[2].version, "2.0.0");
     t.is(packages[3].name, "prod-03");
     t.is(packages[3].version, "3.0.0");
-    t.is(packages[4].name, "prod-02-02");
-    t.is(packages[4].version, "0.0.2");
-    t.is(packages[5].name, "dev-01");
-    t.is(packages[5].version, "0.5.0");
-    t.is(packages[6].name, "dev-02");
-    t.is(packages[6].version, "0.6.0");
+    t.is(packages[4].name, "@comp-01/core");
+    t.is(packages[4].version, "0.5.6");
+    t.is(packages[5].name, "prod-05-05");
+    t.is(packages[5].version, "6.7.8");
+    t.is(packages[6].name, "prod-02-02");
+    t.is(packages[6].version, "0.0.2");
+    t.is(packages[7].name, "prod-04-04");
+    t.is(packages[7].version, "0.3.2");
+    t.is(packages[8].name, "prod-04");
+    t.is(packages[8].version, "1.2.3");
+    t.is(packages[9].name, "dev-01");
+    t.is(packages[9].version, "0.5.0");
+    t.is(packages[10].name, "dev-02");
+    t.is(packages[10].version, "0.6.0");
 });
 
 test.serial("Only production, full installation", async (t) => {
@@ -51,7 +59,7 @@ test.serial("Only production, full installation", async (t) => {
 
     const packages = await getInstalledPackages(path.join(__dirname, "..", "mock-packages", "installation-full"));
 
-    t.is(packages.length, 5);
+    t.is(packages.length, 9);
     t.is(packages[0].name, "prod-01");
     t.is(packages[0].version, "1.0.0");
     t.is(packages[1].name, "prod-02");
@@ -60,8 +68,16 @@ test.serial("Only production, full installation", async (t) => {
     t.is(packages[2].version, "2.0.0");
     t.is(packages[3].name, "prod-03");
     t.is(packages[3].version, "3.0.0");
-    t.is(packages[4].name, "prod-02-02");
-    t.is(packages[4].version, "0.0.2");
+    t.is(packages[4].name, "@comp-01/core");
+    t.is(packages[4].version, "0.5.6");
+    t.is(packages[5].name, "prod-05-05");
+    t.is(packages[5].version, "6.7.8");
+    t.is(packages[6].name, "prod-02-02");
+    t.is(packages[6].version, "0.0.2");
+    t.is(packages[7].name, "prod-04-04");
+    t.is(packages[7].version, "0.3.2");
+    t.is(packages[8].name, "prod-04");
+    t.is(packages[8].version, "1.2.3");
 });
 
 test.serial("Only development, full installation", async (t) => {
