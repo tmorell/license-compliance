5f010672c4538e270a9d85def780485f0406f23d
Teo Morell <teomorell@gmail.com>
2021-03-21 16:07:23 -0400

    Extract await from loop

--- src/npm.ts
+++ src/npm.ts
@@ -11,9 +11,6 @@ const debug = Debug("license-compliance:npm");
 const PACKAGE_JSON = "package.json";
 const NODE_MODULES = "node_modules";
 
-let rootNodeModulesPath: string;
-let packages: Array<Package>;
-
 /**
  * Gets an array of all installed packages, regardless of license being valid.
  *
@@ -21,10 +18,10 @@ let packages: Array<Package>;
  * @returns {Promise<Array<Package>>}
  */
 export async function getInstalledPackages(configuration: Pick<Configuration, "direct" | "development" | "production">, rootPath = ""): Promise<Array<Package>> {
-    packages = new Array<Package>();
+    const packages: Array<Package> = new Array<Package>();
 
     // Paths
-    rootNodeModulesPath = path.join(rootPath, NODE_MODULES);
+    const rootNodeModulesPath = path.join(rootPath, NODE_MODULES);
     const packPath = path.join(rootPath, PACKAGE_JSON);
 
     const pack = await util.readPackageJson(packPath);
@@ -34,11 +31,11 @@ export async function getInstalledPackages(configuration: Pick<Configuration, "d
 
     if (pack.dependencies && !configuration.development) {
         debug("Analyzing production dependencies at", rootNodeModulesPath);
-        await readPackages(pack.name, pack.dependencies, 0, rootNodeModulesPath, configuration);
+        await readPackages(pack.name, pack.dependencies, 0, rootNodeModulesPath, configuration, rootNodeModulesPath, packages);
     }
     if (pack.devDependencies && !configuration.production) {
         debug("Analyzing development dependencies at", rootNodeModulesPath);
-        await readPackages(pack.name, pack.devDependencies, 0, rootNodeModulesPath, configuration);
+        await readPackages(pack.name, pack.devDependencies, 0, rootNodeModulesPath, configuration, rootNodeModulesPath, packages);
     }
 
     return packages;
@@ -51,7 +48,7 @@ export async function getInstalledPackages(configuration: Pick<Configuration, "d
  * @param {Package} pack Package to verify.
  * @returns {boolean} true if it was analyzed; otherwise, false.
  */
-function alreadyAnalyzed(pack: Package): boolean {
+function alreadyAnalyzed(packages: Array<Package>, pack: Package): boolean {
     return packages.find((value) => value.name === pack.name && value.version === pack.version) !== undefined;
 }
 
@@ -65,7 +62,7 @@ function alreadyAnalyzed(pack: Package): boolean {
  * @param {(string | undefined)} parentNodeModulesPath Path of the parent package.
  * @returns {(Promise<string | undefined>)} Promise with the path where the package was found; undefined if not found.
  */
-async function getInstalledPath(parentName: string, packageName: string, parentNodeModulesPath: string): Promise<string | undefined> {
+async function getInstalledPath(parentName: string, packageName: string, parentNodeModulesPath: string, rootNodeModulesPath: string): Promise<string | undefined> {
     // Verify if present in parent's node_modules
     let packagePath = path.join(parentNodeModulesPath, packageName);
     if (await util.fileExists(packagePath)) {
@@ -101,39 +98,49 @@ async function getInstalledPath(parentName: string, packageName: string, parentN
  * @param {string} parentNodeModulesPath Parent package's node_module path.
  * @returns {Promise<void>}
  */
-async function readPackages(parentName: string, dependencies: Array<[string, string]>, depth: number, parentNodeModulesPath: string, configuration: Pick<Configuration, "direct" |"development" | "production">): Promise<void> {
+async function readPackages(parentName: string, dependencies: Array<[string, string]>, depth: number,
+    parentNodeModulesPath: string, configuration: Pick<Configuration, "direct" | "development" | "production">, rootNodeModulesPath: string, packages: Array<Package>,
+): Promise<void> {
     if (depth > 0 && configuration.direct) {
         return;
     }
 
+    const getPackagePromises = new Array<Promise<void>>();
     for (const dependency of Object.keys(dependencies)) {
-        const packagePath = await getInstalledPath(parentName, dependency, parentNodeModulesPath);
-        if (packagePath === undefined) {
-            console.error(chalk.red(`Package "${dependency}" was not found. Confirm that all modules are installed.`));
-            continue;
-        }
-        const file = await util.readPackageJson(path.join(packagePath, PACKAGE_JSON));
-        if (!file) {
-            console.error(chalk.red(`Package "${dependency}" is empty and cannot be analyzed.`));
-            continue;
-        }
-        const license = await getLicense(file, packagePath);
-        const pack: Package = {
-            license: license.name,
-            licenseFile: license.path,
-            name: dependency,
-            path: packagePath,
-            repository: getRepository(file.repository),
-            version: file.version
-        };
-        if (alreadyAnalyzed(pack)) {
-            continue;
-        }
+        getPackagePromises.push(getPackage(parentName, dependency, parentNodeModulesPath, configuration, depth, rootNodeModulesPath, packages));
+    }
+    await Promise.all(getPackagePromises);
+}
 
-        packages.push(pack);
+async function getPackage(parentName: string, dependency: string, parentNodeModulesPath: string,
+    configuration: Pick<Configuration, "direct" | "development" | "production">, depth: number, rootNodeModulesPath: string, packages: Array<Package>
+): Promise<void> {
+    const packagePath = await getInstalledPath(parentName, dependency, parentNodeModulesPath, rootNodeModulesPath);
+    if (packagePath === undefined) {
+        console.error(chalk.red(`Package "${dependency}" was not found. Confirm that all modules are installed.`));
+        return;
+    }
+    const file = await util.readPackageJson(path.join(packagePath, PACKAGE_JSON));
+    if (!file) {
+        console.error(chalk.red(`Package "${dependency}" is empty and cannot be analyzed.`));
+        return;
+    }
+    const license = await getLicense(file, packagePath);
+    const pack: Package = {
+        license: license.name,
+        licenseFile: license.path,
+        name: dependency,
+        path: packagePath,
+        repository: getRepository(file.repository),
+        version: file.version
+    };
+    if (alreadyAnalyzed(packages, pack)) {
+        return;
+    }
 
-        if (file.dependencies) {
-            await readPackages(dependency, file.dependencies, ++depth, path.join(packagePath, NODE_MODULES), configuration);
-        }
+    packages.push(pack);
+
+    if (file.dependencies) {
+        await readPackages(dependency, file.dependencies, depth + 1, path.join(packagePath, NODE_MODULES), configuration, rootNodeModulesPath, packages);
     }
 }
--- tests/npm/getInstalledPackages.spec.ts
+++ tests/npm/getInstalledPackages.spec.ts
@@ -21,90 +21,56 @@ test.serial("Get packages, empty package.json", async (t) => {
 });
 
 test.serial("Get all packages, full installation", async (t) => {
-    // Arguments
     const packages = await getInstalledPackages(getDefaultConfiguration(), path.join(__dirname, "..", "mock-packages", "installation-full"));
 
     t.is(packages.length, 11);
-    t.is(packages[0].name, "prod-01");
-    t.is(packages[0].version, "1.0.0");
-    t.is(packages[0].repository, "https://github.com/user/project");
-    t.is(packages[1].name, "prod-02");
-    t.is(packages[1].version, "2.0.0");
-    t.is(packages[1].repository, Literals.UNKNOWN);
-    t.is(packages[2].name, "prod-02-02");
-    t.is(packages[2].version, "2.0.0");
-    t.is(packages[3].name, "prod-03");
-    t.is(packages[3].version, "3.0.0");
-    t.is(packages[4].name, "@comp-01/core");
-    t.is(packages[4].version, "0.5.6");
-    t.is(packages[5].name, "prod-05-05");
-    t.is(packages[5].version, "6.7.8");
-    t.is(packages[6].name, "prod-02-02");
-    t.is(packages[6].version, "0.0.2");
-    t.is(packages[7].name, "prod-04-04");
-    t.is(packages[7].version, "0.3.2");
-    t.is(packages[8].name, "prod-04");
-    t.is(packages[8].version, "1.2.3");
-    t.is(packages[9].name, "dev-01");
-    t.is(packages[9].version, "0.5.0");
-    t.is(packages[10].name, "dev-02");
-    t.is(packages[10].version, "0.6.0");
+    t.truthy(packages.find(p => p.name === "prod-01" && p.version === "1.0.0" && p.repository === "https://github.com/user/project"));
+    t.truthy(packages.find(p => p.name === "prod-03" && p.version === "3.0.0"));
+    t.truthy(packages.find(p => p.name === "prod-02" && p.version === "2.0.0" && p.repository === Literals.UNKNOWN));
+    t.truthy(packages.find(p => p.name === "@comp-01/core" && p.version === "0.5.6"));
+    t.truthy(packages.find(p => p.name === "prod-02-02" && p.version === "0.0.2"));
+    t.truthy(packages.find(p => p.name === "prod-02-02" && p.version === "2.0.0"));
+    t.truthy(packages.find(p => p.name === "prod-05-05" && p.version === "6.7.8"));
+    t.truthy(packages.find(p => p.name === "prod-04-04" && p.version === "0.3.2"));
+    t.truthy(packages.find(p => p.name === "prod-04" && p.version === "1.2.3"));
+    t.truthy(packages.find(p => p.name === "dev-01" && p.version === "0.5.0"));
+    t.truthy(packages.find(p => p.name === "dev-02" && p.version === "0.6.0"));
 });
 
 test.serial("Only production, full installation", async (t) => {
-    // Arguments
     const packages = await getInstalledPackages(Object.assign(getDefaultConfiguration(), { production: true }),
         path.join(__dirname, "..", "mock-packages", "installation-full"));
 
     t.is(packages.length, 9);
-    t.is(packages[0].name, "prod-01");
-    t.is(packages[0].version, "1.0.0");
-    t.is(packages[1].name, "prod-02");
-    t.is(packages[1].version, "2.0.0");
-    t.is(packages[2].name, "prod-02-02");
-    t.is(packages[2].version, "2.0.0");
-    t.is(packages[3].name, "prod-03");
-    t.is(packages[3].version, "3.0.0");
-    t.is(packages[4].name, "@comp-01/core");
-    t.is(packages[4].version, "0.5.6");
-    t.is(packages[5].name, "prod-05-05");
-    t.is(packages[5].version, "6.7.8");
-    t.is(packages[6].name, "prod-02-02");
-    t.is(packages[6].version, "0.0.2");
-    t.is(packages[7].name, "prod-04-04");
-    t.is(packages[7].version, "0.3.2");
-    t.is(packages[8].name, "prod-04");
-    t.is(packages[8].version, "1.2.3");
+    t.truthy(packages.find(p => p.name === "prod-01" && p.version === "1.0.0"));
+    t.truthy(packages.find(p => p.name === "prod-02" && p.version === "2.0.0"));
+    t.truthy(packages.find(p => p.name === "prod-02-02" && p.version === "2.0.0"));
+    t.truthy(packages.find(p => p.name === "prod-03" && p.version === "3.0.0"));
+    t.truthy(packages.find(p => p.name === "@comp-01/core" && p.version === "0.5.6"));
+    t.truthy(packages.find(p => p.name === "prod-05-05" && p.version === "6.7.8"));
+    t.truthy(packages.find(p => p.name === "prod-02-02" && p.version === "0.0.2"));
+    t.truthy(packages.find(p => p.name === "prod-04-04" && p.version === "0.3.2"));
+    t.truthy(packages.find(p => p.name === "prod-04" && p.version === "1.2.3"));
 });
 
 test.serial("Only development, full installation", async (t) => {
-    // Arguments
     const packages = await getInstalledPackages(Object.assign(getDefaultConfiguration(), { development: true }),
         path.join(__dirname, "..", "mock-packages", "installation-full"));
 
     t.is(packages.length, 3);
-    t.is(packages[0].name, "dev-01");
-    t.is(packages[0].version, "0.5.0");
-    t.is(packages[1].name, "prod-02-02");
-    t.is(packages[1].version, "2.0.0");
-    t.is(packages[2].name, "dev-02");
-    t.is(packages[2].version, "0.6.0");
+    t.truthy(packages.find(p => p.name === "dev-01" && p.version === "0.5.0"));
+    t.truthy(packages.find(p => p.name === "prod-02-02" && p.version === "2.0.0"));
+    t.truthy(packages.find(p => p.name === "dev-02" && p.version === "0.6.0"));
 });
 
 test.serial("Get all packages, full installation, only direct", async (t) => {
-    // Arguments
     const packages = await getInstalledPackages(Object.assign(getDefaultConfiguration(), { direct: true }),
         path.join(__dirname, "..", "mock-packages", "installation-full"));
 
     t.is(packages.length, 5);
-    t.is(packages[0].name, "prod-01");
-    t.is(packages[0].version, "1.0.0");
-    t.is(packages[1].name, "prod-02");
-    t.is(packages[1].version, "2.0.0");
-    t.is(packages[2].name, "prod-03");
-    t.is(packages[2].version, "3.0.0");
-    t.is(packages[3].name, "dev-01");
-    t.is(packages[3].version, "0.5.0");
-    t.is(packages[4].name, "dev-02");
-    t.is(packages[4].version, "0.6.0");
+    t.truthy(packages.find(p => p.name === "prod-01" && p.version === "1.0.0"));
+    t.truthy(packages.find(p => p.name === "prod-02" && p.version === "2.0.0"));
+    t.truthy(packages.find(p => p.name === "prod-03" && p.version === "3.0.0"));
+    t.truthy(packages.find(p => p.name === "dev-01" && p.version === "0.5.0"));
+    t.truthy(packages.find(p => p.name === "dev-02" && p.version === "0.6.0"));
 });
