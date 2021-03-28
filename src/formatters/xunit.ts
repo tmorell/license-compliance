dba7f434230ab3ae6d1f14cdf4c13ccc3c91bd94
Florent Schildknecht <florent.schildknecht@humblebee.se>
2021-03-28 18:41:26 +0200

    feat(formatters): add new xunit formatter
    - add new Xunit formatter (using xmlbuilder internally) with summary and detailed test suites
    - add unit tests for xunit formatter covering all reporters
    - allow "xunit" formatter in configuration validation

---
+++ src/formatters/xunit.ts
@@ -0,0 +1,146 @@
+import xmlbuilder from "xmlbuilder";
+
+import { Formatter } from "./index";
+import { Package } from "../interfaces";
+
+interface LicenseDetail {
+    name: string;
+    packages: Array<Package>;
+}
+
+type XmlValue = string | number | boolean;
+
+type RecursiveXmlValue = XmlValue | Record<string, XmlValue> | Array<Record<string, XmlValue>>;
+
+interface XUnitTestCase extends Record<string, RecursiveXmlValue> {
+    failure: Record<string, XmlValue>;
+}
+
+interface XUnitTestSuite extends Record<string, RecursiveXmlValue | Array<XUnitTestCase>> {
+    testcase: Array<XUnitTestCase>;
+}
+
+interface XUnitTestSuites extends Record<string, RecursiveXmlValue | Array<XUnitTestSuite>> {
+    testsuite: Array<XUnitTestSuite>;
+}
+
+/**
+ * This formatter produces an XUnit compatible format used by some CI/CD engines such as Bitbucket Pipelines
+ * @see https://support.atlassian.com/bitbucket-cloud/docs/test-reporting-in-pipelines/
+ */
+export class Xunit implements Formatter {
+    private readonly TEST_SUITES_NAME = "License Compliance";
+    private readonly TEST_CASE_ERROR_TYPE = "License Compliance Error";
+
+    detail(packages: Array<Package>): void {
+        const xmlContent = this.serializeObjectToXml({
+            testsuites: this.formatDetailedTestSuites(packages),
+        });
+
+        console.info(xmlContent);
+    }
+
+    summary(licenses: Array<{ name: string; count: number }>): void {
+        const xmlContent = this.serializeObjectToXml({
+            testsuites: this.formatSummaryTestSuites(licenses),
+        });
+
+        console.info(xmlContent);
+    }
+
+    private formatDetailedTestSuites(packages: Array<Package>): XUnitTestSuites {
+        const detailedList = this.groupPackagesByLicense(packages);
+        const totalNumberOfFailures = detailedList.reduce((sum, license) => sum + license.packages.length, 0);
+
+        return {
+            "@name": this.TEST_SUITES_NAME,
+            "@tests": totalNumberOfFailures,
+            "@errors": 0,
+            "@failures": totalNumberOfFailures,
+            testsuite: detailedList.map((license) => ({
+                "@name": license.name,
+                "@tests": license.packages.length,
+                "@errors": 0,
+                "@failures": license.packages.length,
+                testcase: license.packages.map((packageInformations) => ({
+                    "@name": `${packageInformations.name}@${packageInformations.version}`,
+                    "@path": packageInformations.path,
+                    failure: {
+                        "@type": this.TEST_CASE_ERROR_TYPE,
+                        "#text": `Package "${packageInformations.name}@${packageInformations.version}" uses non compliant license "${packageInformations.license}"`,
+                    },
+                })),
+            })),
+        };
+    }
+
+    private formatSummaryTestSuites(licenses: Array<{ name: string; count: number }>): XUnitTestSuites {
+        return {
+            "@name": this.TEST_SUITES_NAME,
+            "@tests": licenses.length,
+            "@errors": 0,
+            "@failures": licenses.length,
+            testsuite: licenses.map((license) => ({
+                "@name": license.name,
+                "@tests": 1,
+                "@errors": 0,
+                "@failures": 1,
+                testcase: [{
+                    "@name": license.name,
+                    failure: {
+                        "@type": this.TEST_CASE_ERROR_TYPE,
+                        "#text": `${license.count} package${license.count > 1 ? "s" : ""} use non compliant license "${license.name}"`,
+                    },
+                }],
+            })),
+        };
+    }
+
+    /**
+     * Loop through a list of packages and group them by license name
+     */
+    private groupPackagesByLicense(packages: Array<Package>): Array<LicenseDetail> {
+        return packages.reduce<Array<LicenseDetail>>(
+            (licenses, packageInformations) => {
+                const licenseIndex = licenses.findIndex((license) => license.name === packageInformations.license);
+
+                if (licenseIndex >= 0) {
+                    // if the license was already in the iterator, push the current package to its list
+                    licenses[licenseIndex].packages.push(packageInformations);
+                } else {
+                    // otherwise add an item with the license name
+                    licenses.push({
+                        name: packageInformations.license,
+                        packages: [packageInformations],
+                    });
+                }
+
+                return licenses;
+            },
+            [],
+        );
+    }
+
+    private serializeObjectToXml(object: Record<string, RecursiveXmlValue | XUnitTestSuites>): string {
+        return xmlbuilder
+            .create(object, {
+                stringify: {
+                    attEscape: this.escapeString,
+                    textEscape: this.escapeString,
+                },
+            })
+            .dec("1.0", "UTF-8")
+            .end({
+                pretty: true,
+            });
+    }
+
+    private escapeString(text: string): string {
+        return text
+            .replace(/&/g, "&amp;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&apos;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;");
+    }
+}
---
+++ tests/formatters/xunit.spec.ts
@@ -0,0 +1,67 @@
+import test, { afterEach, beforeEach } from "ava";
+import * as sinon from "sinon";
+
+import { Xunit } from "../../src/formatters/xunit";
+import { Package } from "../../src/interfaces";
+
+let stubConsole: sinon.SinonStub;
+
+beforeEach(() => {
+    stubConsole = sinon.stub(console, "info");
+});
+
+afterEach(() => {
+    sinon.restore();
+});
+
+test.serial("Detailed", (t) => {
+    const packages: Array<Package> = [
+        { name: "pack-01", path: "pack-01", version: "1.1.0", license: "MIT", repository: "company/project" },
+        { name: "pack-02", path: "pack-02", version: "2.2.0", license: "MIT", repository: "company/project2" },
+        { name: "pack-03", path: "pack-03", version: "0.0.3", license: "Unknown", repository: "company/project-3" },
+    ];
+
+    const xunit = new Xunit();
+    xunit.detail(packages);
+
+    t.true(stubConsole.calledWithExactly(`<?xml version="1.0" encoding="UTF-8"?>
+<testsuites name="License Compliance" tests="3" errors="0" failures="3">
+  <testsuite name="MIT" tests="2" errors="0" failures="2">
+    <testcase name="pack-01@1.1.0" path="pack-01">
+      <failure type="License Compliance Error">Package &quot;pack-01@1.1.0&quot; uses non compliant license &quot;MIT&quot;</failure>
+    </testcase>
+    <testcase name="pack-02@2.2.0" path="pack-02">
+      <failure type="License Compliance Error">Package &quot;pack-02@2.2.0&quot; uses non compliant license &quot;MIT&quot;</failure>
+    </testcase>
+  </testsuite>
+  <testsuite name="Unknown" tests="1" errors="0" failures="1">
+    <testcase name="pack-03@0.0.3" path="pack-03">
+      <failure type="License Compliance Error">Package &quot;pack-03@0.0.3&quot; uses non compliant license &quot;Unknown&quot;</failure>
+    </testcase>
+  </testsuite>
+</testsuites>`));
+});
+
+test.serial("Summary", (t) => {
+    const licenses: Array<{ name: string; count: number }> = [
+        { name: "MIT", count: 9 },
+        { name: "Apache-2.0", count: 1 },
+    ];
+
+    const xunit = new Xunit();
+    xunit.summary(licenses);
+
+    t.true(stubConsole.calledWithExactly(`<?xml version="1.0" encoding="UTF-8"?>
+<testsuites name="License Compliance" tests="2" errors="0" failures="2">
+  <testsuite name="MIT" tests="1" errors="0" failures="1">
+    <testcase name="MIT">
+      <failure type="License Compliance Error">9 packages use non compliant license &quot;MIT&quot;</failure>
+    </testcase>
+  </testsuite>
+  <testsuite name="Apache-2.0" tests="1" errors="0" failures="1">
+    <testcase name="Apache-2.0">
+      <failure type="License Compliance Error">1 package use non compliant license &quot;Apache-2.0&quot;</failure>
+    </testcase>
+  </testsuite>
+</testsuites>`));
+});
