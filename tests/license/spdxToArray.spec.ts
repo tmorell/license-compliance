e540db5830b4b5c52ee77bb9a022557d1744e400
Teo Morell <teomorell@gmail.com>
2026-07-17 19:11:47 -0400

    Additional test

--- tests/license/spdxToArray.spec.ts
+++ tests/license/spdxToArray.spec.ts
@@ -10,6 +10,8 @@ import { spdxToArray } from "../../src/license";
     { spdx: "(MIT)", licenses: ["MIT"] },
     { spdx: "(MIT OR ISC)", licenses: ["MIT", "ISC"] },
     { spdx: "(BSD-2-Clause OR MIT OR Apache-2.0)", licenses: ["BSD-2-Clause", "MIT", "Apache-2.0"] },
+    // With -or- as part of name
+    { spdx: "AGPL-1.0-or-later", licenses: ["AGPL-1.0-or-later"] },
     // With extra spaces
     { spdx: " ISC", licenses: ["ISC"] },
     { spdx: "BSD ", licenses: ["BSD"] },
